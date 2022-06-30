68c351303eed3928108d1e3b8fefba9fb6cac087
mkpbd <mostofakamal1990@gmail.com>
2022-06-30 13:05:13 +0600

    type Script basic  Restaction

---
+++ src/typeScriptAction.ts
@@ -0,0 +1,19 @@
+const firstName = "kamal passa";
+// const firstNameLength = firstName.length()
+const firstNameLength = firstName.length
+
+/*
+The code is written in normal JavaScript syntax—I haven’t introduced
+TypeScript-specific syntax yet.
+*/
+
+
+function sayMyName(fullName:string){
+
+    console.log(`Hello Your full name is : ${fullName}`)
+}
+
+sayMyName("mostofa kamal passa");
+
+//  below code show error
+// sayMyName(555)
