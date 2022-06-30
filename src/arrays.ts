db61f2cd71f320a0bbaba83ee7ca0b485368005b
mostofa kamal <mostofakamal1990@gmail.com>
2022-07-01 03:31:39 +0600

    arrays functions

---
+++ src/arrays.ts
@@ -0,0 +1,16 @@
+/* 
+    Arrays and tuples
+    One flexible and one fixed
+    Choose your adventure
+    JavaScript arrays are wildly flexible and can hold any mixture of values inside:
+*/
+const elements = [true, null, undefined, 42];
+// elements.push("even", ["more"]);
+// Value of elements: [true, null, undefined, 42, "even", ["more"]]
+/* 
+    In most cases, though, individual JavaScript arrays are intended to hold
+only one specific type of value. Adding values of a different type may be
+confusing to readers, or worse, the result of an error that could cause
+problems in the program.
+
+*/
